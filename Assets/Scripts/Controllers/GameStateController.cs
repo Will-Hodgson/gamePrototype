08d467135d7b6ca4ad68e661d2c42bd400e2989e
Will-Hodgson <whodgson@ualberta.ca>
2017-05-06 18:39:37 -0600

    Added player and enemy healths and displays on the board

--- Assets/Scripts/Controllers/GameStateController.cs
+++ Assets/Scripts/Controllers/GameStateController.cs
@@ -12,16 +12,32 @@ namespace Assets.Scripts
         private HandController _enemyHandController;
         private DeckController _playerDeckController;
         private DeckController _enemyDeckController;
+        private Text _playerHealthText;
+        private Text _enemyHealthText;
         private Text _playerManaText;
         private Text _enemyManaText;
-        private int _playerMana = 0;
-        private int _playerManaMax = 0;
-        private int _enemyMana = 0;
-        private int _enemyManaMax = 0;
+        private int _playerHealth;
+        private int _enemyHealth;
+        private int _playerMana;
+        private int _playerManaMax;
+        private int _enemyMana;
+        private int _enemyManaMax;
 
         public State turnState { get; set; }
         public State phaseState { get; set; }
         public Transform selectedCard { get; set; }
+
+        public int playerHealth
+        {
+            get { return this._playerHealth; }
+            set { this._playerHealth = value; this.UpdatePlayerHealthText(); }
+        }
+
+        public int enemyHealth
+        {
+            get { return this._enemyHealth; }
+            set { this._enemyHealth = value; this.UpdateEnemyHealthText(); }
+        }
 
         public int playerMana
         {
@@ -57,12 +73,16 @@ namespace Assets.Scripts
             this._playerDeckController = GameObject.Find("PlayerDeckPanel/PlayerDeck").GetComponent<DeckController>();
             this._enemyDeckController = GameObject.Find("EnemyDeckPanel/EnemyDeck").GetComponent<DeckController>();
             this.selectedCard = null;
+            this._playerHealthText = GameObject.Find("PlayerHealth").GetComponent<Text>();
+            this._enemyHealthText = GameObject.Find("EnemyHealth").GetComponent<Text>();
             this._playerManaText = GameObject.Find("PlayerMana").GetComponent<Text>();
             this._enemyManaText = GameObject.Find("EnemyMana").GetComponent<Text>();
         }
 
         void Start()
         {
+            this._playerHealth = this._enemyHealth = 25;
+            this._playerMana = this._enemyMana = this._playerManaMax = this._enemyManaMax = 0;
             this._playerDeckController.Init(Enumerable.Repeat("TestUnit", 30).ToList());
             this._enemyDeckController.Init(Enumerable.Repeat("TestUnit", 30).ToList());
             this.phaseState.Enter();
@@ -91,14 +111,24 @@ namespace Assets.Scripts
             this.phaseState.Execute();
         }
 
+        private void UpdatePlayerHealthText()
+        {
+            this._playerHealthText.text = "Health: " + this._playerHealth.ToString();
+        }
+
+        private void UpdateEnemyHealthText()
+        {
+            this._enemyHealthText.text = "Health: " + this._enemyHealth.ToString();
+        }
+
         private void UpdatePlayerManaText()
         {
-            this._playerManaText.text = "Mana:" + this._playerMana.ToString() + "/" + this._playerManaMax.ToString();
+            this._playerManaText.text = "Mana: " + this._playerMana.ToString() + "/" + this._playerManaMax.ToString();
         }
 
         private void UpdateEnemyManaText()
         {
-            this._enemyManaText.text = "Mana:" + this._enemyMana.ToString() + "/" + this._enemyManaMax.ToString();
+            this._enemyManaText.text = "Mana: " + this._enemyMana.ToString() + "/" + this._enemyManaMax.ToString();
         }
 
         public void ColorPlayableAndMovableCards()
